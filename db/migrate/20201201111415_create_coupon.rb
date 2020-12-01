bf5d6a18aec22f4f7fec81bf116d983da6d4e899
josue <josuevelezdr@gmail.com>
2020-12-01 12:30:13 +0100

    adding taken field to coupon table

--- db/migrate/20201201111415_create_coupon.rb
+++ db/migrate/20201201111415_create_coupon.rb
@@ -2,6 +2,7 @@ class CreateCoupon < ActiveRecord::Migration[6.0]
   def change
     create_table :coupons do |t|
       t.string :code, null: false, index: true
+      t.boolean :taken, default: false
     end
   end
 end
